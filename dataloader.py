3644c880bdbc407ac8d4086924778e55c9e51758
Gustav Larsen <redgustavlarsen@gmail.com>
2020-03-26 15:02:26 +0100

    Dataloader done?

--- dataloader.py
+++ dataloader.py
@@ -1,1 +1,44 @@
+import os, sys
+import gzip
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+import urllib.request
+
+
+
+def load_mnist(path, kind='train'):
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte.gz'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte.gz'
+                               % kind)
+
+    with gzip.open(labels_path, 'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
+                               offset=8)
+
+    with gzip.open(images_path, 'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
+                               offset=16).reshape(len(labels), 784)
+
+    return images, labels
+
+
+X_train, y_train = load_mnist('data', kind='train')
+X_test, y_test = load_mnist('data', kind='t10k')
+
+trainidx = np.arange(len(X_train),dtype=np.int)
+
+order = np.random.permutation(range(len(X_train)))
+
+poolnum = 20000
+poolset = order[:poolnum]
+
+Xpool = np.take(X_train,poolset,axis=0)
+ypool = np.take(y_train,poolset,axis=0)
+
+trainidx=np.setdiff1d(trainidx,poolset)
 
