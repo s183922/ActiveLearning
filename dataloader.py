b3fb63efbb37c56d2245edc52e76b43c701b47df
Gustav Larsen <redgustavlarsen@gmail.com>
2020-03-28 12:48:21 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -26,19 +26,22 @@ def load_mnist(path, kind='train'):
 
     return images, labels
 
+def datasets(path):
+    #input: Path as string, should be 'data'
+    X_train, y_train = load_mnist(path, kind='train')
+    X_test, y_test = load_mnist(path, kind='t10k')
 
-X_train, y_train = load_mnist('data', kind='train')
-X_test, y_test = load_mnist('data', kind='t10k')
+    trainidx = np.arange(len(X_train),dtype=np.int)
 
-trainidx = np.arange(len(X_train),dtype=np.int)
+    order = np.random.permutation(range(len(X_train)))
 
-order = np.random.permutation(range(len(X_train)))
+    poolnum = 20000
+    poolset = order[:poolnum]
 
-poolnum = 20000
-poolset = order[:poolnum]
+    #Take and define the pool from the trainingset
+    Xpool = np.take(X_train,poolset,axis=0)
+    ypool = np.take(y_train,poolset,axis=0)
 
-Xpool = np.take(X_train,poolset,axis=0)
-ypool = np.take(y_train,poolset,axis=0)
+    trainidx = np.setdiff1d(trainidx,poolset)
 
-trainidx=np.setdiff1d(trainidx,poolset)
-
+    return X_train, y_train, X_test, y_test, Xpool, ypool, trainidx
